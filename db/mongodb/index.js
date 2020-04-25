242ddfae494bcda65eb1e0576acd2a702427089d
Susy(Qinqin) Yang <susyyang24@gmail.com>
2020-04-25 00:14:49 -0400

    test

--- db/mongodb/index.js
+++ db/mongodb/index.js
@@ -30,9 +30,9 @@
 
 
 // const mongoose = require('mongoose');
-// if(process.env.MONGODB_URI){
+// if (process.env.MONGODB_URI) {
 //   mongoose.connect(process.env.MONGODB_URI)
-// } else{
+// } else {
 //   // mongoose.connect('mongodb://localhost:27017/badmovies', { useNewUrlParser: true });
 //   mongoose.connect('mongodb://localhost/badmovies', { useNewUrlParser: true });
 
